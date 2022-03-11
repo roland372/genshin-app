ff42b448ea1ae6820225c03f7f2ee575a1d162cb
roland372 <damfat94@gmail.com>
2022-03-11 02:39:37 +0100

    added character talent leveling up

--- src/components/FarmingPlanner/CharacterCard.js
+++ src/components/FarmingPlanner/CharacterCard.js
@@ -6,173 +6,280 @@ import talentOptions from './talentOptions';
 import { characterLevelUp } from './Materials';
 import { talentLevelUp } from './Materials';
 
-// console.log(characterLevelUp[13].materials[0]);
-// console.log(talentLevelUp[9].materials[0].mora);
-
 const CharacterCard = props => {
 	const { charactersData, characters } = props;
 
 	// NA - normal attack
 	// ES - elemental skill
 	// EB - elemental burst
-	const [levelLow, setLevelLow] = useState(1);
-	const [levelHigh, setLevelHigh] = useState(1);
-	const [NALow, setNALow] = useState(1);
-	const [NAHigh, setNAHigh] = useState(1);
-	const [ESLow, setESLow] = useState(1);
-	const [ESHigh, setESHigh] = useState(1);
-	const [EBLow, setEBLow] = useState(1);
-	const [EBHigh, setEBHigh] = useState(1);
+	let [levelLow, setLevelLow] = useState(1);
+	let [levelHigh, setLevelHigh] = useState(1);
+	let [NALow, setNALow] = useState(1);
+	let [NAHigh, setNAHigh] = useState(1);
+	let [ESLow, setESLow] = useState(1);
+	let [ESHigh, setESHigh] = useState(1);
+	let [EBLow, setEBLow] = useState(1);
+	let [EBHigh, setEBHigh] = useState(1);
 	const [characterSelect, setCharacterSelect] = useState('');
 	// console.log('Level: ', levelLow, levelHigh);
 	// console.log('NA: ', NALow, NAHigh);
 	// console.log('ES: ', ESLow, ESHigh);
 	// console.log('EB: ', EBLow, EBHigh);
 
-	const [mora, setMora] = useState(0);
+	// <----- character level up state ----->
+	const [moraCharacter, setMoraCharacter] = useState(0);
 	const [expBooks, setExpBooks] = useState(0);
 	const [localSpeciality, setLocalSpeciality] = useState(0);
-	const [commonMaterial1, setCommonMaterial1] = useState(0);
-	const [commonMaterial2, setCommonMaterial2] = useState(0);
-	const [commonMaterial3, setCommonMaterial3] = useState(0);
+	const [characterCommonMaterial1, setCharacterCommonMaterial1] = useState(0);
+	const [characterCommonMaterial2, setCharacterCommonMaterial2] = useState(0);
+	const [characterCommonMaterial3, setCharacterCommonMaterial3] = useState(0);
 	const [sliver, setSliver] = useState(0);
 	const [fragments, setFragments] = useState(0);
 	const [chunks, setChunks] = useState(0);
 	const [gemstones, setGemstones] = useState(0);
 	const [bossAscensionMaterial, setBossAscensionMaterial] = useState(0);
-	console.log(characterLevelUp[10].materials);
-	console.log(characterLevelUp[10].materials[2].localSpeciality);
 
+	// <----- talent level up state ----->
+	const [moraTalent, setMoraTalent] = useState(0);
+	const [bronzeTalentBooks, setBronzeTalentBooks] = useState(0);
+	const [silverTalentBooks, setSilverTalentBooks] = useState(0);
+	const [goldTalentBooks, setGoldTalentBooks] = useState(0);
+	const [talentCommonMaterial1, setTalentCommonMaterial1] = useState(0);
+	const [talentCommonMaterial2, setTalentCommonMaterial2] = useState(0);
+	const [talentCommonMaterial3, setTalentCommonMaterial3] = useState(0);
+	const [bossMaterial, setBossMaterial] = useState(0);
+	const [crown, setCrown] = useState(0);
+
+	const setCharacterLevelUp = () => {
+		setMoraCharacter(
+			characterLevelUp[levelHigh - 1].materials[0].mora -
+				characterLevelUp[levelLow - 1].materials[0].mora
+		);
+		setExpBooks(
+			characterLevelUp[levelHigh - 1].materials[1].expBooks -
+				characterLevelUp[levelLow - 1].materials[1].expBooks
+		);
+		setLocalSpeciality(
+			characterLevelUp[levelHigh - 1].materials[2].localSpeciality -
+				characterLevelUp[levelLow - 1].materials[2].localSpeciality
+		);
+		setCharacterCommonMaterial1(
+			characterLevelUp[levelHigh - 1].materials[3].commonMaterial1 -
+				characterLevelUp[levelLow - 1].materials[3].commonMaterial1
+		);
+		setCharacterCommonMaterial2(
+			characterLevelUp[levelHigh - 1].materials[4].commonMaterial2 -
+				characterLevelUp[levelLow - 1].materials[4].commonMaterial2
+		);
+		setCharacterCommonMaterial3(
+			characterLevelUp[levelHigh - 1].materials[5].commonMaterial3 -
+				characterLevelUp[levelLow - 1].materials[5].commonMaterial3
+		);
+		setSliver(
+			characterLevelUp[levelHigh - 1].materials[6].sliver -
+				characterLevelUp[levelLow - 1].materials[6].sliver
+		);
+		setFragments(
+			characterLevelUp[levelHigh - 1].materials[7].fragments -
+				characterLevelUp[levelLow - 1].materials[7].fragments
+		);
+		setChunks(
+			characterLevelUp[levelHigh - 1].materials[8].chunks -
+				characterLevelUp[levelLow - 1].materials[8].chunks
+		);
+		setGemstones(
+			characterLevelUp[levelHigh - 1].materials[9].gemstones -
+				characterLevelUp[levelLow - 1].materials[9].gemstones
+		);
+		setBossAscensionMaterial(
+			characterLevelUp[levelHigh - 1].materials[10].bossAscensionMaterial -
+				characterLevelUp[levelLow - 1].materials[10].bossAscensionMaterial
+		);
+	};
+
+	const setTalentLevelUp = () => {
+		setMoraTalent(
+			talentLevelUp[NAHigh - 1].materials.mora -
+				talentLevelUp[NALow - 1].materials.mora +
+				(talentLevelUp[ESHigh - 1].materials.mora -
+					talentLevelUp[ESLow - 1].materials.mora) +
+				(talentLevelUp[EBHigh - 1].materials.mora -
+					talentLevelUp[EBLow - 1].materials.mora)
+		);
+		setBronzeTalentBooks(
+			talentLevelUp[NAHigh - 1].materials.bronzeTalentBooks -
+				talentLevelUp[NALow - 1].materials.bronzeTalentBooks +
+				(talentLevelUp[ESHigh - 1].materials.bronzeTalentBooks -
+					talentLevelUp[ESLow - 1].materials.bronzeTalentBooks) +
+				(talentLevelUp[EBHigh - 1].materials.bronzeTalentBooks -
+					talentLevelUp[EBLow - 1].materials.bronzeTalentBooks)
+		);
+		setSilverTalentBooks(
+			talentLevelUp[NAHigh - 1].materials.silverTalentBooks -
+				talentLevelUp[NALow - 1].materials.silverTalentBooks +
+				(talentLevelUp[ESHigh - 1].materials.silverTalentBooks -
+					talentLevelUp[ESLow - 1].materials.silverTalentBooks) +
+				(talentLevelUp[EBHigh - 1].materials.silverTalentBooks -
+					talentLevelUp[EBLow - 1].materials.silverTalentBooks)
+		);
+		setGoldTalentBooks(
+			talentLevelUp[NAHigh - 1].materials.goldTalentBooks -
+				talentLevelUp[NALow - 1].materials.goldTalentBooks +
+				(talentLevelUp[ESHigh - 1].materials.goldTalentBooks -
+					talentLevelUp[ESLow - 1].materials.goldTalentBooks) +
+				(talentLevelUp[EBHigh - 1].materials.goldTalentBooks -
+					talentLevelUp[EBLow - 1].materials.goldTalentBooks)
+		);
+		setTalentCommonMaterial1(
+			talentLevelUp[NAHigh - 1].materials.commonMaterial1 -
+				talentLevelUp[NALow - 1].materials.commonMaterial1 +
+				(talentLevelUp[ESHigh - 1].materials.commonMaterial1 -
+					talentLevelUp[ESLow - 1].materials.commonMaterial1) +
+				(talentLevelUp[EBHigh - 1].materials.commonMaterial1 -
+					talentLevelUp[EBLow - 1].materials.commonMaterial1)
+		);
+		setTalentCommonMaterial2(
+			talentLevelUp[NAHigh - 1].materials.commonMaterial2 -
+				talentLevelUp[NALow - 1].materials.commonMaterial2 +
+				(talentLevelUp[ESHigh - 1].materials.commonMaterial2 -
+					talentLevelUp[ESLow - 1].materials.commonMaterial2) +
+				(talentLevelUp[EBHigh - 1].materials.commonMaterial2 -
+					talentLevelUp[EBLow - 1].materials.commonMaterial2)
+		);
+		setTalentCommonMaterial3(
+			talentLevelUp[NAHigh - 1].materials.commonMaterial3 -
+				talentLevelUp[NALow - 1].materials.commonMaterial3 +
+				(talentLevelUp[ESHigh - 1].materials.commonMaterial3 -
+					talentLevelUp[ESLow - 1].materials.commonMaterial3) +
+				(talentLevelUp[EBHigh - 1].materials.commonMaterial3 -
+					talentLevelUp[EBLow - 1].materials.commonMaterial3)
+		);
+		setBossMaterial(
+			talentLevelUp[NAHigh - 1].materials.bossMaterial -
+				talentLevelUp[NALow - 1].materials.bossMaterial +
+				(talentLevelUp[ESHigh - 1].materials.bossMaterial -
+					talentLevelUp[ESLow - 1].materials.bossMaterial) +
+				(talentLevelUp[EBHigh - 1].materials.bossMaterial -
+					talentLevelUp[EBLow - 1].materials.bossMaterial)
+		);
+		setCrown(
+			talentLevelUp[NAHigh - 1].materials.crown -
+				talentLevelUp[NALow - 1].materials.crown +
+				(talentLevelUp[ESHigh - 1].materials.crown -
+					talentLevelUp[ESLow - 1].materials.crown) +
+				(talentLevelUp[EBHigh - 1].materials.crown -
+					talentLevelUp[EBLow - 1].materials.crown)
+		);
+	};
+
+	//? <----- character level up switch ----->
 	useEffect(() => {
 		switch (levelHigh) {
 			case 1:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
-				setExpBooks(
-					characterLevelUp[levelHigh - 1].materials[1].expBooks -
-						characterLevelUp[levelLow - 1].materials[1].expBooks
-				);
-				setLocalSpeciality(
-					characterLevelUp[levelHigh - 1].materials[2].localSpeciality -
-						characterLevelUp[levelLow - 1].materials[2].localSpeciality
-				);
-				setCommonMaterial1(
-					characterLevelUp[levelHigh - 1].materials[3].commonMaterial1 -
-						characterLevelUp[levelLow - 1].materials[3].commonMaterial1
-				);
-				setCommonMaterial2(
-					characterLevelUp[levelHigh - 1].materials[4].commonMaterial2 -
-						characterLevelUp[levelLow - 1].materials[4].commonMaterial2
-				);
-				setCommonMaterial3(
-					characterLevelUp[levelHigh - 1].materials[5].commonMaterial3 -
-						characterLevelUp[levelLow - 1].materials[5].commonMaterial3
-				);
-				setSliver(
-					characterLevelUp[levelHigh - 1].materials[6].sliver -
-						characterLevelUp[levelLow - 1].materials[6].sliver
-				);
-				setFragments(
-					characterLevelUp[levelHigh - 1].materials[7].fragments -
-						characterLevelUp[levelLow - 1].materials[7].fragments
-				);
-				setChunks(
-					characterLevelUp[levelHigh - 1].materials[8].chunks -
-						characterLevelUp[levelLow - 1].materials[8].chunks
-				);
-				setGemstones(
-					characterLevelUp[levelHigh - 1].materials[9].gemstones -
-						characterLevelUp[levelLow - 1].materials[9].gemstones
-				);
-				setBossAscensionMaterial(
-					characterLevelUp[levelHigh - 1].materials[10].bossAscensionMaterial -
-						characterLevelUp[levelLow - 1].materials[10].bossAscensionMaterial
-				);
+				setCharacterLevelUp();
 				break;
 			case 2:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 3:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 4:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 5:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 6:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 7:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 8:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 9:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 10:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 11:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 12:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 13:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			case 14:
-				setMora(
-					characterLevelUp[levelHigh - 1].materials[0].mora -
-						characterLevelUp[levelLow - 1].materials[0].mora
-				);
+				setCharacterLevelUp();
 				break;
 			default:
 				break;
 		}
-	}, [levelHigh, levelLow]);
+	});
+
+	//? <----- talent level up switch ----->
+	useEffect(() => {
+		switch (NAHigh || ESHigh || EBHigh) {
+			case 1:
+				setTalentLevelUp();
+				break;
+			case 2:
+				setTalentLevelUp();
+				break;
+			case 3:
+				setTalentLevelUp();
+				break;
+			case 4:
+				setTalentLevelUp();
+				break;
+			case 5:
+				setTalentLevelUp();
+				break;
+			case 6:
+				setTalentLevelUp();
+				break;
+			case 7:
+				setTalentLevelUp();
+				break;
+			case 8:
+				setTalentLevelUp();
+				break;
+			case 9:
+				setTalentLevelUp();
+				break;
+			case 10:
+				setTalentLevelUp();
+				break;
+			default:
+				break;
+		}
+	});
+
+	// <----- prevent negative select values ----->
+	if (levelLow > levelHigh) {
+		levelHigh = levelLow;
+		setLevelHigh(levelHigh);
+	}
+
+	if (NALow > NAHigh) {
+		NAHigh = NALow;
+		setNAHigh(NAHigh);
+	}
+
+	if (ESLow > ESHigh) {
+		ESHigh = ESLow;
+		setESHigh(ESHigh);
+	}
+
+	if (EBLow > EBHigh) {
+		EBHigh = EBLow;
+		setEBHigh(EBHigh);
+	}
 
 	// select array
 	const characterNames = [];
@@ -455,7 +562,7 @@ const CharacterCard = props => {
 							className='btn btn-sm btn-light'
 							onClick={() => {
 								setLevelLow(1);
-								setLevelHigh(80);
+								setLevelHigh(12);
 								setNALow(1);
 								setNAHigh(8);
 								setESLow(1);
@@ -475,7 +582,7 @@ const CharacterCard = props => {
 					<section className='border d-flex flex-wrap' key={c.data.name}>
 						<div>
 							<img src='/images/Materials/Item_Mora.png' width='50px' alt='' />
-							<div>{mora}</div>
+							<div>{moraCharacter + moraTalent}</div>
 						</div>
 						<div>
 							<img
@@ -499,7 +606,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>{commonMaterial1}</div>
+							<div>{characterCommonMaterial1 + talentCommonMaterial1}</div>
 						</div>
 						<div>
 							<img
@@ -507,7 +614,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>{commonMaterial2}</div>
+							<div>{characterCommonMaterial2 + talentCommonMaterial2}</div>
 						</div>
 						<div>
 							<img
@@ -515,7 +622,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>{commonMaterial3}</div>
+							<div>{characterCommonMaterial3 + talentCommonMaterial3}</div>
 						</div>
 						<div>
 							<img
@@ -563,7 +670,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>200</div>
+							<div>{bronzeTalentBooks}</div>
 						</div>
 						<div>
 							<img
@@ -571,7 +678,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>200</div>
+							<div>{silverTalentBooks}</div>
 						</div>
 						<div>
 							<img
@@ -579,7 +686,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>200</div>
+							<div>{goldTalentBooks}</div>
 						</div>
 						<div>
 							<img
@@ -587,7 +694,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>200</div>
+							<div>{bossMaterial}</div>
 						</div>
 						<div>
 							<img
@@ -595,7 +702,7 @@ const CharacterCard = props => {
 								width='50px'
 								alt=''
 							/>
-							<div>200</div>
+							<div>{crown}</div>
 						</div>
 					</section>
 				) : (
