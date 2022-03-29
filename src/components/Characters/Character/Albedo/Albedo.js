09509ece12c6f7928e046017c2ec397ff0b1f62d
roland372 <damfat94@gmail.com>
2022-03-29 17:11:18 +0200

    backup before restructuring caracters section

--- src/components/Characters/Character/Albedo/Albedo.js
+++ src/components/Characters/Character/Albedo/Albedo.js
@@ -14,7 +14,6 @@ import Ascensions from '../../Ascensions';
 import data from '../../../../assets/data/Characters/Albedo.json';
 
 const Albedo = () => {
-	// const path = process.env.PUBLIC_URL;
 
 	const {
 		name,
