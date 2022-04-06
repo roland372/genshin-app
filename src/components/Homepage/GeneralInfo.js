0ebea10a023e908bc24c92f6d51eceeb9176f4ca
roland372 <damfat94@gmail.com>
2022-04-06 20:46:17 +0200

    adjusted font size

--- src/components/Homepage/GeneralInfo.js
+++ src/components/Homepage/GeneralInfo.js
@@ -4,12 +4,12 @@ import CardComponent from '../Layout/CardComponent';
 const GeneralInfo = () => {
 	return (
 		<CardComponent title='Welcome to Genshn-App'>
-			<p>
-				Genshin-App is a fanmade website for Genshin Impact game. Database where
-				you can have easy and quick access to most useful information like
-				current and past events or banners, see what's available in domains,
-				quickly calculate materials needed for leveling up or even build your
-				own teams.
+			<p className='mx-3'>
+				Genshin-App is a fanmade website for Genshin Impact game. It has
+				database where you can have easy and quick access to most useful
+				information like current and past events or banners, see what's
+				available in domains, quickly calculate materials needed for leveling up
+				or even build your own teams.
 			</p>
 		</CardComponent>
 	);
