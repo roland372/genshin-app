17475b2b2c4960b802ede5c57ec6bbd18c7936b6
roland372 <damfat94@gmail.com>
2022-05-06 00:55:01 +0200

    adjusted styling

--- src/components/Characters/Ascension.js
+++ src/components/Characters/Ascension.js
@@ -6,41 +6,41 @@ const Ascension = props => {
 			<th scope='row'>{props.ascensionPhase}</th>
 			<td>{props.level}</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-3'>
+				<div className='d-flex align-items-center mx-2'>
 					<img src={props.ascensionMaterialImage} width='64px' alt='' />
 					<span>{props.ascensionMaterialName}</span>
-					<span className='rounded px-1 py-2 ms-2'>
+					<span className='rounded px-1 py-2 mx-2'>
 						{props.ascensionMaterialAmount}
 					</span>
 				</div>
 			</td>
 			<td>
-				<div className='d-flex align-items-center ms-3'>
+				<div className='d-flex align-items-center mx-2'>
 					<img src={props.bossMaterialImage} width='64px' alt='' />
 					<span>{props.bossMaterialName}</span>
-					<span className='rounded p-2 ms-2'>{props.bossMaterialAmount}</span>
+					<span className='rounded p-2 mx-2'>{props.bossMaterialAmount}</span>
 				</div>
 			</td>
 			<td>
-				<div className='d-flex align-items-center ms-3'>
+				<div className='d-flex align-items-center mx-2'>
 					<img src={props.localSpecialityImage} width='64px' alt='' />
 					<span>{props.localSpecialityName}</span>
-					<span className='rounded p-2 ms-2'>
+					<span className='rounded p-2 mx-2'>
 						{props.localSpecialityAmount}
 					</span>
 				</div>
 			</td>
 			<td>
-				<div className='d-flex align-items-center ms-3'>
+				<div className='d-flex align-items-center mx-2'>
 					<img src={props.commonMaterialImage} width='64px' alt='' />
 					<span>{props.commonMaterialName}</span>
-					<span className='rounded p-2 ms-2'>{props.commonMaterialAmount}</span>
+					<span className='rounded p-2 mx-2'>{props.commonMaterialAmount}</span>
 				</div>
 			</td>
 			<td>
-				<div className='d-flex align-items-center ms-3'>
+				<div className='d-flex align-items-center mx-2'>
 					<img src='images/Materials/Item_Mora.png' width='64px' alt='' />
-					<span className='rounded p-2 ms-2'>{props.moraCost}</span>
+					<span className='rounded p-2 mx-2'>{props.moraCost}</span>
 				</div>
 			</td>
 		</tr>
