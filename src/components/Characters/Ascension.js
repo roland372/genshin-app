71f4ca6703be5455e40d05766b3fcec340a088fc
roland372 <damfat94@gmail.com>
2022-05-06 11:39:55 +0200

    adjusted styling of tables

--- src/components/Characters/Ascension.js
+++ src/components/Characters/Ascension.js
@@ -6,30 +6,38 @@ const Ascension = props => {
 			<th scope='row'>{props.ascensionPhase}</th>
 			<td>{props.level}</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center mx-2'>
-					<img src={props.ascensionMaterialImage} width='64px' alt='' />
-					<span>{props.ascensionMaterialName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center mx-2'>
+						<img src={props.ascensionMaterialImage} width='64px' alt='' />
+						<span>{props.ascensionMaterialName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.ascensionMaterialAmount}</span>
 				</div>
 			</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-2'>
-					<img src={props.bossMaterialImage} width='64px' alt='' />
-					<span>{props.bossMaterialName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center ms-2'>
+						<img src={props.bossMaterialImage} width='64px' alt='' />
+						<span>{props.bossMaterialName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.bossMaterialAmount}</span>
 				</div>
 			</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-2'>
-					<img src={props.localSpecialityImage} width='64px' alt='' />
-					<span>{props.localSpecialityName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center ms-2'>
+						<img src={props.localSpecialityImage} width='64px' alt='' />
+						<span>{props.localSpecialityName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.localSpecialityAmount}</span>
 				</div>
 			</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-2'>
-					<img src={props.commonMaterialImage} width='64px' alt='' />
-					<span>{props.commonMaterialName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center ms-2'>
+						<img src={props.commonMaterialImage} width='64px' alt='' />
+						<span>{props.commonMaterialName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.commonMaterialAmount}</span>
 				</div>
 			</td>
--- src/components/Characters/TalentMaterial.js
+++ src/components/Characters/TalentMaterial.js
@@ -4,32 +4,39 @@ const TalentMaterial = props => {
 	return (
 		<tr>
 			<th scope='row'>{props.level}</th>
-			{/* justify-content-between */}
 			<td className='text-start'>
-				<div className='d-flex align-items-center mx-2'>
-					<img src={props.talentBookImage} width='64px' alt='' />
-					<span>{props.talentBookName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center mx-2'>
+						<img src={props.talentBookImage} width='64px' alt='' />
+						<span>{props.talentBookName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.talentBookAmount}</span>
 				</div>
 			</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-2'>
-					<img src={props.commonMaterialImage} width='64px' alt='' />
-					<span>{props.commonMaterialName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center ms-2'>
+						<img src={props.commonMaterialImage} width='64px' alt='' />
+						<span>{props.commonMaterialName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.commonMaterialAmount}</span>
 				</div>
 			</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-2'>
-					<img src={props.bossMaterialImage} width='64px' alt='' />
-					<span>{props.bossMaterialName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center ms-2'>
+						<img src={props.bossMaterialImage} width='64px' alt='' />
+						<span>{props.bossMaterialName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.bossMaterialAmount}</span>
 				</div>
 			</td>
 			<td className='text-start'>
-				<div className='d-flex align-items-center ms-2'>
-					<img src={props.crownImage} width='64px' alt='' />
-					<span>{props.crownName}</span>
+				<div className='d-flex justify-content-between align-items-center'>
+					<div className='d-flex align-items-center ms-2'>
+						<img src={props.crownImage} width='64px' alt='' />
+						<span>{props.crownName}</span>
+					</div>
 					<span className='p-2 mx-2'>{props.crownAmount}</span>
 				</div>
 			</td>
