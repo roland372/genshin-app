f49f081b1f04d4c108cd50fdc2e5543d6cb68601
roland372 <damfat94@gmail.com>
2022-05-07 12:55:38 +0200

    tested changing style dynamically

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -14,8 +14,23 @@ import useDocumentTitle from '../../hooks/useDocumentTitle';
 const Homepage = props => {
 	useDocumentTitle('Home');
 
+	// const color = getComputedStyle(document.documentElement).getPropertyValue(
+	// 	'--test'
+	// );
+
+	// console.log(color);
+
+	// const setColor = newColor => {
+	// 	document.documentElement.style.setProperty('--test', newColor);
+	// };
+
 	return (
 		<Container>
+			{/* <button onClick={() => setColor('#12232e')}>1</button>
+			<button onClick={() => setColor('#203647')}>2</button>
+			<button onClick={() => setColor('#284155')}>3</button>
+			<button onClick={() => setColor('#023e8a')}>4</button>
+			<button onClick={() => setColor('#4da8da')}>5</button> */}
 			<GeneralInfo />
 			<CharacterDomains />
 			<WeaponDomains />
