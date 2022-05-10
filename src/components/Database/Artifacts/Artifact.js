3b1263a63cb4e93024182601cb4b7a47f8f1144e
roland372 <damfat94@gmail.com>
2022-05-10 14:22:32 +0200

    added background colors depending on rarity

--- src/components/Database/Artifacts/Artifact.js
+++ src/components/Database/Artifacts/Artifact.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
-const Artifact = props => {
+const Artifact = ({ name, rarity, image, bonus1, bonus2 }) => {
+	let rarityColor = '';
+	if (rarity === '5★') {
+		rarityColor = 'rarity5bg';
+	} else if (rarity === '4★') {
+		rarityColor = 'rarity4bg';
+	} else if (rarity === '3★') {
+		rarityColor = 'rarity3bg';
+	}
+
 	return (
 		<tr className='new-line align-middle text-color'>
-			<th scope='row'>{props.name}</th>
-			<td>{props.rarity}</td>
+			<th scope='row'>{name}</th>
+			<td>{rarity}</td>
 			<td>
-				<img className='img' src={props.image} width='50px' alt='' />
+				<img
+					className={`${rarityColor} rounded img-fluid`}
+					src={image}
+					width='50px'
+					alt=''
+				/>
 			</td>
 			<td className='text-start'>
 				<div>
 					<b>2 Piece: </b>
-					{props.bonus1}
+					{bonus1}
 				</div>
 				<div>
 					<b>4 Piece: </b>
-					{props.bonus2}
+					{bonus2}
 				</div>
 			</td>
 		</tr>
